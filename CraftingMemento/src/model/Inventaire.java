2bfa595f2323a0505fb24a41622cc83a65251dd4
Pyeroh <quentin.nanteuil@gmail.com>
2014-01-18 11:23:20 +1100

    Inventaire : ensemble d'items avec une quantité

---
+++ CraftingMemento/src/model/Inventaire.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Inventaire extends ArrayList<Item> {
+
+	private static final long serialVersionUID = -9121171867762237055L;
+	
+	
+
+}
