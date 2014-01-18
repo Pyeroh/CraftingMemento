de8cb5e1d308b8722b48f745520e16663e655ccb
Pyeroh <quentin.nanteuil@gmail.com>
2014-01-18 11:24:01 +1100

    Définition d'énumérations

---
+++ CraftingMemento/src/model/enums/ERecetteForme.java
@@ -0,0 +1,8 @@
+package model.enums;
+
+public enum ERecetteForme {
+
+	forme,
+	sansforme;
+	
+}
---
+++ CraftingMemento/src/model/enums/ERecetteType.java
@@ -0,0 +1,9 @@
+package model.enums;
+
+public enum ERecetteType {
+
+	craft,
+	four,
+	alambic;
+	
+}
