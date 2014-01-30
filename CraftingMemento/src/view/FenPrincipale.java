631c4b165a13626dc512bd4b535417d32ae91e0d
Pyeroh <quentin.nanteuil@gmail.com>
2014-01-30 16:02:43 +1100

    Initialisation de la fenêtre principale

---
+++ CraftingMemento/src/view/FenPrincipale.java
@@ -0,0 +1,18 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class FenPrincipale extends JFrame {
+
+	private static final long serialVersionUID = 2540259731678095668L;
+	
+	public FenPrincipale() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+		setVisible(true);
+		setSize(415,314);
+		
+	}
+
+}
--- CraftingMemento/src/view/Launch.java
+++ CraftingMemento/src/view/Launch.java
@@ -1,12 +1,17 @@
 package view;
 
+import javax.swing.UIManager;
+
 import model.Recette;
 
 
 public class Launch {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		System.out.println(Recette.gres);
+		
+		UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+		new FenPrincipale();
 	}
 
 }
