99cc04bf96ed6b05cfd0c7d0f351ca2c2f5c0964
Pyeroh <quentin.nanteuil@gmail.com>
2014-03-04 06:01:46 +1100

    Corrections

--- CraftingMemento/src/view/Launch.java
+++ CraftingMemento/src/view/Launch.java
@@ -2,19 +2,23 @@ package view;
 
 import javax.swing.UIManager;
 
+import model.enums.Recette;
+
 
 public class Launch {
 
 	public static void main(String[] args) throws Exception {
-		/*ArrayList<Recette> recettes = Recette.getRecettes(EItem.white_wool);
-		for(int i=0;i<recettes.size();i++) {
-			System.out.println(recettes.get(i).toString());
-		}*/
-		
-		/*ArrayList<EItem> searches = EItem.searchBy("r");
-		for (EItem eItem : searches) {
-			System.out.println(eItem.getGuiName());
-		}*/
+		Recette[] values = Recette.values();
+		int max = 0;
+		for (int i=0;i<values.length-1;i++) {
+			Recette recette = values[i];
+			try {
+				max = recette.getItem().getQuantite()>max ? recette.getItem().getQuantite() : max;
+			} catch (Exception e) {
+				throw new ArrayIndexOutOfBoundsException(recette.toString());
+			}
+		}
+		System.out.println(max);
 		
 		UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
 		new FenPrincipale();
