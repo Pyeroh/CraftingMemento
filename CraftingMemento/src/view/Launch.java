b278953c9ec1899be0e2537f91f0fe37f0b024c4
Pyeroh <quentin.nanteuil@gmail.com>
2014-03-17 15:00:52 +1100

    Travail en cours sur le Tooltip dynamique

--- CraftingMemento/src/view/Launch.java
+++ CraftingMemento/src/view/Launch.java
@@ -1,10 +1,8 @@
 package view;
 
-import java.awt.Color;
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 
-import javax.swing.BorderFactory;
 import javax.swing.UIManager;
 
 
@@ -12,23 +10,13 @@ public class Launch {
 
 	public static void main(String[] args) throws Exception {
 
-		Font mcFont = Font.createFont(Font.TYPE1_FONT, Launch.class.getResourceAsStream("/gui/MinecraftFont.pfa"));
-		//Font mcFont = Font.createFont(Font.TRUETYPE_FONT, Launch.class.getResourceAsStream("/gui/MinecraftFont.ttf"));
-		GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(mcFont);
-
-		/*UIManager.put("info", new Color(29,14,29));
-		UIManager.put("ToolTip.font", mcFont.deriveFont(Font.PLAIN).deriveFont(16f));
-		UIManager.put("ToolTip.border", BorderFactory.createEmptyBorder(10, 10, 10, 10));*/
+		Font minecraftia = Font.createFont(Font.TRUETYPE_FONT, Launch.class.getResourceAsStream("/gui/Minecraftia.ttf"));
+		GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(minecraftia);
 
 		UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
 
-
-
-
-
-
-		new FenPrincipale();
-		//new FenetreTest_XSwing();
+		//new FenPrincipale();
+		new FenetreTest_XSwing();
 
 	}
 
