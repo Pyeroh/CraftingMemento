cd2e6db546f4b8eb938dd90ca5c5aa6eb903ae34
Pyeroh <quentin.nanteuil@gmail.com>
2014-04-08 17:13:11 +1100

    Ajout d'une liste d'ingrédients et nettoyage

---
+++ CraftingMemento/src/model/Ingredients.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.util.ArrayList;
+
+import model.enums.EItem;
+
+public class Ingredients extends ArrayList<Item> {
+
+	private static final long serialVersionUID = 8982033548147523728L;
+
+	public boolean containsEItem(EItem e) {
+		return indexOf(e) != -1;
+	}
+
+	/**
+	 * @see Ingredients#indexOf(Object)
+	 * @param e
+	 * @return
+	 */
+	public int indexOf(EItem e) {
+		int i = 0;
+		if (size() == 0) return -1;
+		while (get(i).getItem() != e && i < size() - 1) {
+			i++;
+		}
+		if (get(i).getItem() == e) {
+			return i;
+		}
+		else return -1;
+	}
+
+}
