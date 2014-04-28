431199b7be2df5e7757022d5f44ab19cf878c1b6
Pyeroh <quentin.nanteuil@gmail.com>
2014-04-28 16:00:50 +1100

    -

--- CraftingMemento/src/view/Launch.java
+++ CraftingMemento/src/view/Launch.java
@@ -2,11 +2,9 @@ package view;
 
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
+import java.util.Locale;
 
 import javax.swing.UIManager;
-
-import model.enums.Recette;
-
 
 public class Launch {
 
@@ -17,41 +15,10 @@ public class Launch {
 
 		UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
 
-		//new FenPrincipale();
-		//new FenetreTest_XSwing();
+		System.out.println(Locale.FRENCH.getLanguage());
 
-		/*EItem[] values = EItem.values();
-		LinkedHashMap<EItem, ArrayList<Recette>> associations = new LinkedHashMap<>();
-
-		for (EItem eItem : values) {
-			ArrayList<Recette> recettes = Recette.getRecettes(eItem);
-			@SuppressWarnings("unchecked")
-			ArrayList<Recette> recettes2 = (ArrayList<Recette>) recettes.clone();
-			for (Recette recette : recettes) {
-				if (recette.getItem().getItem() != eItem) {
-					recettes2.remove(recette);
-				}
-			}
-			if (recettes2.size() >= 2) {
-				associations.put(eItem, recettes2);
-			}
-		}
-
-		for (Iterator<EItem> it = associations.keySet().iterator(); it.hasNext();) {
-			EItem type = (EItem) it.next();
-			System.out.println(type);
-			System.out.println(associations.get(type));
-			System.out.println();
-		}*/
-
-//		EItemInfo[] values = EItemInfo.values();
-//		for (EItemInfo eItemInfo : values) {
-//			System.out.println(eItemInfo.getItem().getGuiName() + " est primaire ? " + eItemInfo.isPrimaire());
-//		}
-
-		for (Recette recette : Recette.values()) {
-			System.out.println(recette.getItem().getItem().getGuiName() + " x5 : " + Recette.calcule(recette, 5, true));
-		}
+		// new FenPrincipale();
+		// new FenetreTest_XSwing();
 
 	}
 
